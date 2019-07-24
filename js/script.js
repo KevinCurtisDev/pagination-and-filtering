7db6e64f76f45bbc3341bc99aa994a0ff08ace20
kevin <kpcurtis@gmail.com>
2019-07-24 13:41:15 +0100

    Added pagination functionality

--- js/script.js
+++ js/script.js
@@ -6,19 +6,9 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const page = document.querySelector(".page");
-let studentList = Array.from(document.getElementsByClassName("student-item"));
-let studentListVal = studentList.length;
+const studentList = Array.from(document.getElementsByClassName("student-item"));
+const studentListVal = studentList.length;
 
 //create a variable to store the number of pagination buttons needed
 let paginationButtonNum = Math.ceil(studentListVal/10);
@@ -40,10 +30,12 @@ const showPage = (pageNumber) => {
        that will be passed into the parens later when you call or
        "invoke" the function
    ***/
+   //Hide all student items
    for (let i = 0; i < studentList.length; i++) {
       studentList[i].classList.add("student-item-hide");
    }
 
+  //show student items between the page number and the next page number
   for(let i = 0; i < studentList.length; i++){
      if (studentList.indexOf(studentList[i]) >= pageNumber && studentList.indexOf(studentList[i]) < pageNumber+10) {
         studentList[i].classList.remove("student-item-hide");
@@ -61,7 +53,7 @@ const appendPageLinks = () => {
    let paginationLinks = document.createElement('div');
    paginationLinks.classList.add('pagination');
 
-   for (let i = 1; i < paginationButtonNum; i++){
+   for (let i = 1; i <= paginationButtonNum; i++){
       let link = document.createElement('a');
       let button = document.createElement('li');
       button.innerHTML = i;
@@ -72,6 +64,8 @@ const appendPageLinks = () => {
    page.appendChild(paginationLinks);
    paginationLinks.addEventListener('click', (e) => {
       let num = Number(e.target.innerHTML + 0);
+      num -= 10;
+
       showPage(num);
    });
 }
