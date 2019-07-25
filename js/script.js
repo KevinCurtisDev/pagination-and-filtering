bac7e91700981901eed83fb7fe6c185a5654d2b9
kevin <kpcurtis@gmail.com>
2019-07-25 22:12:14 +0100

    completed search/filtering functionality

--- js/script.js
+++ js/script.js
@@ -8,13 +8,20 @@ FSJS project 2 - List Filter and Pagination
 
 const page = document.querySelector(".page");
 const pageHeader = document.querySelector(".page-header");
+
+//Initial list of students
 const studentList = Array.from(document.querySelectorAll(".student-item"));
-const studentsPerPage = 10;
-const studentListVal = studentList.length;
+let activeStudentList = [];
+
 const studentSearchBox = document.querySelector('#student-search-box');
 const studentNames = document.querySelectorAll(".student-item .student-details h3");
-let paginationButtonNum = Math.ceil(studentListVal / 10);//store the number of pagination buttons needed
+const studentsPerPage = 10;
+let activeCount = studentList.length;
+let paginationButtonNum = Math.ceil(activeCount / studentsPerPage);
 const searchBarContainer = document.createElement('div');
+let paginationLinks = document.createElement('div');
+paginationLinks.classList.add('pagination');
+
 
 /***************************************************************************/
 
@@ -32,55 +39,55 @@ searchBarContainer.innerHTML = searchBar;
 
 //Append the search bar to the page header
 pageHeader.append(searchBarContainer);
-/*************************************************************************/
 
 
-//display first 10 items on initial page load
-for (let i = 0; i < studentList.length; i++) {
-   if (studentList.indexOf(studentList[i]) >= studentsPerPage) {
-      studentList[i].classList.add("student-item-hide");
+
+
+/****************************INITIAL DISPLAY *****************************/
+
+const initialDisplay = () => {
+   for (let i = 0; i < studentList.length; i++) {
+      //Add a data attribute indicating that all student elements are active
+      studentList[i].setAttribute("data", "active");
+      activeStudentList.push(studentList[i]);
+
+      if (studentList.indexOf(studentList[i]) >= studentsPerPage) {
+         studentList[i].classList.add("student-item-hide");
+      }
    }
 }
 
-//filter function
-pageHeader.addEventListener('input', (e) => {
-   studentNames.forEach(name => {
-      if (!name.innerHTML.includes(e.target.value)) {
-         name.parentElement.parentElement.classList.add("student-item-hide");
-      } else {
-         name.parentElement.parentElement.classList.remove("student-item-hide");
-      }
-   })
-});
+initialDisplay();
 
+
+/************************* DISPLAY STUDENTS ON SPECIFIC PAGES **********************/
 
 const showPage = (pageNumber) => { 
    //Hide all student items
-   for (let i = 0; i < studentList.length; i++) {
-      studentList[i].classList.add("student-item-hide");
-   }
+   for (let i = 0; i < activeStudentList.length; i++) {
+      //Hide all student elements
+      activeStudentList[i].classList.add("student-item-hide");
 
-  //show student items between the page number and the next page number
-  for(let i = 0; i < studentList.length; i++){
-     if (studentList.indexOf(studentList[i]) >= pageNumber && studentList.indexOf(studentList[i]) < pageNumber + studentsPerPage) {
-        studentList[i].classList.remove("student-item-hide");
-     } 
-  }
-   
+      //conditional statement for displaying specific set of students
+      if (activeStudentList.indexOf(activeStudentList[i]) >= pageNumber 
+         && activeStudentList.indexOf(activeStudentList[i]) < pageNumber + studentsPerPage) {
+         activeStudentList[i].classList.remove("student-item-hide");
+      } 
+   }
 }
 
-//Create and append page link buttons
-const appendPageLinks = () => {
-   let paginationLinks = document.createElement('div');
-   paginationLinks.classList.add('pagination');
+/*************************** CREATE & ADD PAGE LINK BUTTONS ************************/
+const appendPageLinks = (num) => {
 
-   for (let i = 1; i <= paginationButtonNum; i++){
+   paginationLinks.innerHTML = "";
+
+   for (let i = 1; i <= num; i++){
       let button = document.createElement('li');
       button.setAttribute("class", "link");
       button.innerHTML = i;
       paginationLinks.appendChild(button);
    }
-   
+
    page.appendChild(paginationLinks);
    paginationLinks.addEventListener('click', (e) => {
       let num = Number(e.target.innerHTML + 0);
@@ -90,4 +97,53 @@ const appendPageLinks = () => {
    });
 }
 
-appendPageLinks();
+//call appendPageLinks for initial onload 
+appendPageLinks(paginationButtonNum);
+
+
+/************************* FILTER THE LIST FOR SEARCHED NAMES **************************/
+
+pageHeader.addEventListener('input', (e) => {
+
+   //Set active student list to empty
+   activeStudentList = [];
+
+   activeCount = studentList.length;
+   studentNames.forEach(name => {
+
+      //add data active attribute to each student item
+      name.parentElement.parentElement.setAttribute("data", "active");
+      //populate active student list with all students
+      activeStudentList.push(name.parentElement.parentElement);
+
+      //check if each student matches the search parameter
+      if (!name.innerHTML.includes(e.target.value)) {
+         //hide non matching items and remove active attribute
+         name.parentElement.parentElement.classList.add("student-item-hide");
+         name.parentElement.parentElement.removeAttribute("data");
+         
+         //remove non matches from the active student list
+         activeStudentList.pop(activeStudentList[activeStudentList.indexOf(name.parentElement.parentElement)]);
+      } else {
+         //otherwise show the item
+         name.parentElement.parentElement.classList.remove("student-item-hide");
+      }
+   });
+
+   //reset the page links container to empty
+   paginationLinks.innerHTML = "";
+   paginationButtonNum = Math.ceil(activeStudentList.length / studentsPerPage);
+   //Add links based on new list of students
+   appendPageLinks(paginationButtonNum);
+
+   //Finally loop through active student list and only display at most 10 students per page
+   for (let i = 0; i < activeStudentList.length; i++) {
+      if (activeStudentList.indexOf(activeStudentList[i]) >= studentsPerPage) {
+         activeStudentList[i].classList.add("student-item-hide");
+      }
+   }
+});
+
+
+
+appendPageLinks(paginationButtonNum);
